81aec5f8960b5a99b86e17de99bb4c2b59b7f6a2
Gabriel Amorim <gabriel._amorim@hotmail.com>
2021-11-02 17:51:01 -0300

    Correção css

--- src/Pages/CreateLocation/Index.tsx
+++ src/Pages/CreateLocation/Index.tsx
@@ -1,8 +1,8 @@
 import React, { ChangeEvent, FormEvent, useCallback, useEffect, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
-import { FiArrowLeft } from 'react-icons/fi';
-import { Map, TileLayer, Marker } from 'react-leaflet';
-import { LeafletEvent, LeafletMouseEvent } from 'leaflet'
+import {FiArrowLeft} from 'react-icons/fi';
+import { Map, TileLayer, Marker} from 'react-leaflet';
+import { LeafletMouseEvent } from 'leaflet';
 import api from '../../services/api';
 import Dropzone from '../../components/Dropzone';
 import logo from '../../assets/logo.svg';
@@ -17,15 +17,15 @@ interface Item {
 const CreateLocation: React.FC = () => {
     const [items, setItems] = useState<Item[]>([]);
 
-    const [selectMapPosition, setSelectedMapPosition] = useState<[number, number]>([0, 0]);
-   
+    const [selectedMapPosition, setSelectedMapPosition] = useState<[number, number]>([0, 0]);
+
     const [formData, setFormData] = useState({
         name: '',
         email: '',
         whatsapp: '',
         city: '',
         uf: '',
-    })
+    });
 
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
 
@@ -33,11 +33,11 @@ const CreateLocation: React.FC = () => {
 
     const history = useHistory();
 
-    useEffect(() => { 
+    useEffect(() => {
         api.get('items').then(response => {
             setItems(response.data);
-        })
-    },[]);
+        });
+    }, []);
 
     const handleMapClick = useCallback((event: LeafletMouseEvent): void => {
         //console.log(event);
@@ -47,48 +47,51 @@ const CreateLocation: React.FC = () => {
         ]);
     }, []);
 
-    const handleInputCHange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    const handleInputChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
         //console.log(event.target.name, event.target.value);
         const { name, value } = event.target;
         setFormData({...formData, [name]: value });
     }, [formData]);
 
-    const handleSelectItem = useCallback((id: number) =>{
-        //setSecletedItems([... selectedItems, id]);
+    const handleSelectItem = useCallback((id: number) => {
+        //setSelectedItems([...selectedItems, id]);
         const alreadySelected = selectedItems.findIndex(item => item === id);
 
-        if(alreadySelected >= 0){
+        if (alreadySelected >= 0) {
             const filteredItems = selectedItems.filter(item => item !== id);
             setSelectedItems(filteredItems);
         } else {
-            setSelectedItems([... selectedItems, id]);
+            setSelectedItems([...selectedItems, id]);
         }
     }, [selectedItems]);
 
-    const handleSubmit = useCallback(async(event: FormEvent) =>{
+    const handleSubmit = useCallback(async (event: FormEvent) => {
         event.preventDefault();
 
         const {city, email, name, uf, whatsapp} = formData;
-        const [latitude, longitude] = selectMapPosition;
+        const [latitude, longitude] = selectedMapPosition;
         const items = selectedItems;
 
-        const data = {
-            city,
-            email,
-            name,
-            uf,
-            whatsapp,
-            latitude,
-            longitude,
-            items,
-        };
-        
+        const data = new FormData();
+
+        data.append('name', name);
+        data.append('email', email);
+        data.append('whatsapp', whatsapp);
+        data.append('uf', uf);
+        data.append('city', city);
+        data.append('latitude', String(latitude));
+        data.append('longitude', String(longitude));
+        data.append('items', items.join(','));
+        if (selectedFile) {
+            data.append('image', selectedFile);
+        }
+
         await api.post('locations', data);
 
         alert('Estabelecimento cadastrado com sucesso!');
 
         history.push('/');
-    }, [formData, selectedItems, setSelectedMapPosition])
+    }, [formData, selectedItems, selectedMapPosition, history, selectedFile]);
 
     return (
         <div id="page-create-location">
@@ -96,27 +99,28 @@ const CreateLocation: React.FC = () => {
                 <header>
                     <img src={logo} alt="Coleta Seletiva"/>
                     <Link to="/">
-                        <FiArrowLeft/>
-                        Voltar para Home
+                        <FiArrowLeft />
+                        Voltar para home
                     </Link>
                 </header>
 
                 <form onSubmit={handleSubmit}>
-                    <h1>Cadastro do <br/> local de coleta</h1>
-                <fieldset>
-                    <legend>
-                        <h2>Dados</h2>
-                    </legend>
+                    <h1>Cadastro do <br /> local de coleta</h1>
 
-                    <Dropzone onFileUploaded={setSelectedFile} />
+                    <fieldset>
+                        <legend>
+                            <h2>Dados</h2>
+                        </legend>
 
-                    <div className="field">
+                        <Dropzone onFileUploaded={setSelectedFile} />
+
+                        <div className="field">
                             <label htmlFor="name">Nome da entidade</label>
                             <input 
                                 type="text"
                                 name="name"
                                 id="name"
-                                onChange={handleInputCHange}
+                                onChange={handleInputChange}
                             />
                         </div>
                         <div className="field-group">
@@ -126,7 +130,7 @@ const CreateLocation: React.FC = () => {
                                     type="email"
                                     name="email"
                                     id="email"
-                                    onChange={handleInputCHange}
+                                    onChange={handleInputChange}
                                 />
                             </div>
                             <div className="field">
@@ -135,32 +139,32 @@ const CreateLocation: React.FC = () => {
                                     type="text"
                                     name="whatsapp"
                                     id="whatsapp"
-                                    onChange={handleInputCHange}
+                                    onChange={handleInputChange}
                                 />
                             </div>
                         </div>
-                </fieldset>
+                    </fieldset>
 
-                <fieldset>
-                    <legend>
+                    <fieldset>
+                        <legend>
                         <h2>Endereço</h2>
-                        <span>Marque o endereço no mapa</span>
-                    </legend>
-                    <Map center={[-23.0003709, -43.365895]} zoom={14} onclick={handleMapClick}>
-                    <TileLayer
-                        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    />
-                        <Marker position={selectMapPosition}/>
-                    </Map>
-                    <div className="field-group">
+                            <span>Marque o endereço no mapa</span>
+                        </legend>
+                        <Map center={[-23.0003709, -43.365895]} zoom={14} onClick={handleMapClick}>
+                            <TileLayer
+                                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+                            <Marker position={selectedMapPosition} />
+                        </Map>
+                        <div className="field-group">
                             <div className="field">
                                 <label htmlFor="city">Cidade</label>
                                 <input 
                                     type="text"
                                     name="city"
                                     id="city"
-                                    onChange={handleInputCHange}
+                                    onChange={handleInputChange}
                                 />
                             </div>
                             <div className="field">
@@ -169,32 +173,33 @@ const CreateLocation: React.FC = () => {
                                     type="text"
                                     name="uf"
                                     id="uf"
-                                    onChange={handleInputCHange}
+                                    onChange={handleInputChange}
                                 />
                             </div>
                         </div>
                     </fieldset>
 
-                <fieldset>
-                    <legend>
-                        <h2>Ítens coletados</h2>
-                        <span>Você pode marcar um ou mais items</span>
-                    </legend>
-                <ul className="items-grid">
-                    {items.map(item => (
-                    <li 
-                    key={item.id} 
-                    onClick={() => handleSelectItem(item.id)}
-                    className={selectedItems.includes(item.id) ? 'selected' : ''}
-                    >
-                    <img src="{item.image_url}" alt={item.title}/>    
-                    </li>
-                    ))}
-                </ul>
-                </fieldset>
-                <button type='submit'>
-                    Cadastrar local de coleta
-                </button>
+                    <fieldset>
+                        <legend>
+                            <h2>Ítens coletados</h2>
+                            <span>Você pode marcar um ou mais ítens</span>
+                        </legend>
+                        <ul className="items-grid">
+                            {items.map(item => (
+                                <li
+                                    key={item.id}
+                                    onClick={() => handleSelectItem(item.id)}
+                                    className={selectedItems.includes(item.id) ? 'selected' : ''}
+                                >
+                                    <img src={item.image_url} alt={item.title} />
+                                </li>
+                            ))}
+                        </ul>
+                    </fieldset>
+
+                    <button type="submit">
+                        Cadastrar local de coleta
+                    </button>
                 </form>
             </div>
         </div>
